0a3524f294a71909f9a2fecb09194a2c2f43d439
Baldwin Browne <Rooke83@users.noreply.github.com>
2015-05-03 16:10:56 -0700

    Added a save to file functionality. Notes should persist now.
    Still need to implement the dave edit and delete feature. Which might require changing the way long press passes Intent.

--- app/src/main/java/com/example/baldwin/notetaker/MainActivity.java
+++ app/src/main/java/com/example/baldwin/notetaker/MainActivity.java
@@ -17,6 +17,12 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,14 +36,17 @@ public class MainActivity extends ActionBarActivity {
     private ArrayAdapter<String> adapter;
     private ListView listView;
     private EditText editText;
+    private String filename = "savedNotes";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         context = this;
+
         storeNotes = new ArrayList<>();
         editText = (EditText) findViewById(R.id.enter_text);
 
@@ -66,7 +75,7 @@ public class MainActivity extends ActionBarActivity {
         if (note != null) startActivity(intent);
     }
 
-    public void addItem(View view) {
+    public void saveNote(View view) {
         String input = editText.getText().toString();
         if (input.length() > 0) {
             adapter.add(input);
@@ -74,6 +83,58 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    public void populateArray() {
+        File file = new File(this.getFilesDir(), filename);
+        if (file.exists()) {
+            try {
+                FileInputStream input = openFileInput(filename);
+                DataInputStream din = new DataInputStream(input);
+                int size = din.readInt();
+                for (int i = 0; i < size; i++) {
+                    String line = din.readUTF();
+
+                    storeNotes.add(line);
+
+                }
+                din.close();
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        populateArray();
+        adapter = new ArrayAdapter<>(context,
+                android.R.layout.simple_list_item_1, storeNotes);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        try {
+            FileOutputStream output = openFileOutput(filename, this.MODE_PRIVATE);
+            DataOutputStream out = new DataOutputStream(output);
+            out.writeInt(storeNotes.size());
+            for (String line : storeNotes) {
+                out.writeUTF(line);
+            }
+            out.flush();
+            out.close();
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        storeNotes.clear();
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
